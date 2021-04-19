ba50c622c49faf18d4fd69a95ace61f6ecf4fa65
Roman Dibikhin <dibikhin@gmail.com>
2021-04-19 18:45:07 +0300

    saying bye added

--- client.go
+++ client.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"google.golang.org/grpc"
@@ -18,6 +22,8 @@ const (
 func main() {
 	log.Print("Starting client...")
 
+	onExit(sayBye)
+
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -25,13 +31,13 @@ func main() {
 	log.Print("Dialed address")
 	defer conn.Close()
 
-	c := pb.NewGameClient(conn)
+	cli := pb.NewGameClient(conn)
 	log.Print("Connected client")
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	err = statusLoop(c, ctx)
+	err = statusLoop(cli, ctx)
 	if err != nil {
 		log.Fatalf("could not get status: %v", err)
 	}
@@ -44,11 +50,14 @@ func statusLoop(c pb.GameClient, ctx context.Context) error {
 		return err
 	}
 	log.Printf("GetStatus(): %v", r)
-	act(r)
+	err = act(r)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func act(r *pb.StatusReply) {
+func act(r *pb.StatusReply) error {
 	switch r.Action {
 	case "do auth":
 		fmt.Println("Are you Player1 or Player2?")
@@ -56,5 +65,23 @@ func act(r *pb.StatusReply) {
 		fmt.Println("Choose your mark: 'X' or 'O'")
 	case "ask turn":
 		fmt.Println("Player 123, choose turn from 1 to 9:")
+	default:
+		return errors.New("unknown action: " + r.Action)
 	}
+	return nil
+}
+
+func onExit(done func()) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func(f func()) {
+		<-c
+		f()
+		os.Exit(0)
+	}(done)
+}
+
+func sayBye() {
+	fmt.Println()
+	fmt.Println("Bye!")
 }
